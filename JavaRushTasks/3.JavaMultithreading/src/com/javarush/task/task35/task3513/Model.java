7b59d16c103c4fa0f432419ad32808308b18313e
Antonov Maxim <atnimak@gmail.com>
2021-03-19 20:17:31 +0300

    2048 - 3

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,4 +1,14 @@
 package com.javarush.task.task35.task3513;
 
 public class Model {
+    private static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+    public Model(){
+        for(int i = 0;i<FIELD_WIDTH;i++){
+            for(int k = 0;k<FIELD_WIDTH;k++){
+                gameTiles[i][k] = new Tile();
+            }
+        }
+
+    }
 }
