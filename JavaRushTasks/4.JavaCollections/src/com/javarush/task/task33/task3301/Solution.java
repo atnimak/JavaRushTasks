3b7e069278f0def7b93c7f2534d8c9a2b93e4685
Antonov Maxim <atnimak@gmail.com>
2023-04-11 22:54:10 +0400

    reanimated project, added libraries, solved some tasks

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task33/task3301/Solution.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task33/task3301/Solution.java
@@ -1,8 +1,6 @@
 package com.javarush.task.task33.task3301;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
